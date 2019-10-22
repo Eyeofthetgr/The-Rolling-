b8fe8bac201dee986206d4ac951a9321ed03eeba
Kamil Borkowski <kamil2000-22@o2.pl>
2019-10-22 18:36:37 +0200

    TRS

---
+++ scripts.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal('.banner', {
+    reset: true,
+    delay: 300,
+    distance: 0
+});
+sr.reveal('.firstrow', {
+    reset: true,
+    delay: 400,
+    distance: 0
+});
+sr.reveal('.secondrow', {
+    reset: true,
+    delay: 500,
+    distance: 0
+});
+sr.reveal('.thirdrow', {
+    reset: true,
+    delay: 300,
+    distance: 0
+});
